db918fb4caf08ea7ac7b6638c44166a9405b87bb
Belkis Guanipa <belkisguanipa12@gmail.com>
2025-05-09 13:47:12 -0300

    [Update] Mejora en Upload file:permite subir carpetas con archivos

--- __init__.py
+++ __init__.py
@@ -44,32 +44,50 @@ if module == "setCredentials":
         print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
         PrintException()
         raise e
-
+    
 if module == "uploadFile":
-    source_file_name = GetParams("file_path")
+    source_file_name = GetParams("file_path")  # Puede ser archivo, lista de archivos, o carpeta
     bucket_name = GetParams("bucket_name")
-    destination_blob_name = GetParams("destination_blob_name")
+    destination_blob_name = GetParams("destination_blob_name")  # Puede ser nombre único o carpeta destino
     timeout = GetParams("timeout")
     if not timeout:
         timeout = 1000
+
     try:
-        """Uploads a file to the bucket."""
-        # bucket_name = "your-bucket-name"
-        # source_file_name = "local/path/to/file"
-        # destination_blob_name = "storage-object-name"
         storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
         storage_client = storage.Client()
         bucket = storage_client.bucket(bucket_name)
-        blob = bucket.blob(destination_blob_name)
-        blob.upload_from_filename(source_file_name, timeout=int(timeout))
-        blob.chunk_size = 5 * 1024 * 1024
-        print(
-            "File {} uploaded to {}.".format(
-                source_file_name, destination_blob_name
-            )
-        )
+
+        if os.path.isdir(source_file_name):
+            # Subir todos los archivos de la carpeta
+            for root, _, files in os.walk(source_file_name):
+                for file in files:
+                    full_path = os.path.join(root, file)
+                    rel_path = os.path.relpath(full_path, start=source_file_name)
+                    blob_path = os.path.join(destination_blob_name, rel_path).replace("\\", "/")
+                    blob = bucket.blob(blob_path)
+                    blob.chunk_size = 5 * 1024 * 1024
+                    blob.upload_from_filename(full_path, timeout=int(timeout))
+                    print("File {} uploaded to {}.".format(full_path, blob_path))
+        else:
+            # Carga múltiple por coma
+            source_list = [x.strip() for x in source_file_name.split(',')]
+            destination_list = [x.strip() for x in destination_blob_name.split(',')] if destination_blob_name else []
+
+            if len(destination_list) == 1 and len(source_list) > 1:
+                destination_list = [destination_list[0]] * len(source_list)
+
+            if destination_list and len(source_list) != len(destination_list):
+                raise ValueError("La cantidad de archivos y nombres de destino no coincide.")
+
+            for i, src in enumerate(source_list):
+                dst = destination_list[i] if destination_list else os.path.basename(src)
+                blob = bucket.blob(dst)
+                blob.chunk_size = 5 * 1024 * 1024
+                blob.upload_from_filename(src, timeout=int(timeout))
+                print("File {} uploaded to {}.".format(src, dst))
 
     except Exception as e:
-        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
+        print("\x1B[31;40mAn error occurred\x1B[0m")
         PrintException()
         raise e
